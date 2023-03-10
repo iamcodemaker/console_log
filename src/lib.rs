3f2d71bcfc2935e82e8a9a76a7ee2fd81633f9f1
Matthew Nicholson <matt@matt-land.com>
2023-03-09 23:00:29 -0500

    bump version numbers to 0.2.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/console_log/0.2.0")]
+#![doc(html_root_url = "https://docs.rs/console_log/0.2.2")]
 
 //! A logger that logs to the browser's console.
 //!
